e71f06ebaf61c4c9dcfe1a17e998751c5f1693da
Chandrahasa Sujal <chanducodemonk@outlook.com>
2019-11-20 23:59:15 +0530

    Reception Controller and its views are added.

---
+++ ClinicManagement/CM.Web/Areas/Admin/Controllers/ReceptionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CM.Web.Areas.Admin.Controllers
+{
+    public class ReceptionController : Controller
+    {
+        // GET: Admin/Reception
+        public ActionResult Appointment()
+        {
+            return View();
+        }
+
+        public ActionResult ReceptionView()
+        {
+            return View();
+        }
+    }
+}
