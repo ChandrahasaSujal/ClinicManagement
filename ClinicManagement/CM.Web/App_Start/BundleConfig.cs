7399e905f8dd462c83266394f3e96fe159ff882f
Chandrahasa Sujal <chanducodemonk@outlook.com>
2019-12-07 22:44:50 +0530

    bundles are re arrenged.

--- ClinicManagement/CM.Web/App_Start/BundleConfig.cs
+++ ClinicManagement/CM.Web/App_Start/BundleConfig.cs
@@ -8,20 +8,44 @@ namespace CM.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/assets/styles").Include(
-                        "~/Content/bootstrap.min.css",
-                        "~/assets/css/jquery-ui.min.css",
-                        "~/assets/css/style.css",
+            #region styles
+
+            bundles.Add(new StyleBundle("~/assets/bootstrapCss").Include(
+                        "~/Content/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/jqueryUICss").Include(
+                        "~/assets/css/jquery-ui.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/jqueryDatatablesCss").Include(
                         "~/assets/css/jquery.dataTables.min.css",
-                        "~/assets/css/dataTables.bootstrap4.min.css",
+                        "~/assets/css/dataTables.bootstrap4.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/styles").Include(
+                        "~/assets/css/style.css",
                         "~/assets/vendors/mdi/css/materialdesignicons.min.css",
                         "~/assets/vendors/css/vendor.bundle.base.css"));
-            bundles.Add(new ScriptBundle("~/assets/scripts").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/assets/js/jquery-ui.min.js",
-                "~/assets/vendors/js/vendor.bundle.base.js",
+
+            #endregion
+
+            #region scripts
+
+            bundles.Add(new ScriptBundle("~/assets/jqueryJs").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/jqueryUIJs").Include(
+               "~/assets/js/jquery-ui.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/datatablesJs").Include(
+                "~/assets/vendors/js/vendor.bundle.base.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/vendorJs").Include(
+                "~/assets/js/jquery.dataTables.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/datatablesJs").Include(
                 "~/assets/js/jquery.dataTables.min.js",
-                "~/assets/js/dataTables.bootstrap4.min.js",
+                "~/assets/js/dataTables.bootstrap4.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/scripts").Include(
                 "~/assets/js/off-canvas.js",
                 "~/assets/js/hoverable-collapse.js",
                 "~/assets/js/misc.js",
@@ -35,8 +59,10 @@ namespace CM.Web
             bundles.Add(new ScriptBundle("~/assets/datePickerScripts").Include(
                 "~/assets/js/jquery-ui.js"
                 ));
-                #if DEBUG
-                            BundleTable.EnableOptimizations = false;
+
+            #endregion
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
                 #else
                             BundleTable.EnableOptimizations = true;
                 #endif
