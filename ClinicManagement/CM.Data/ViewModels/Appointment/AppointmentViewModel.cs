010c2efb456c728d31af8eee303aa9187a27f781
Chandrahasa Sujal <chanducodemonk@outlook.com>
2019-12-21 12:15:28 +0530

    Progile pic added and Medicine controller has created.

--- ClinicManagement/CM.Data/ViewModels/Appointment/AppointmentViewModel.cs
+++ ClinicManagement/CM.Data/ViewModels/Appointment/AppointmentViewModel.cs
@@ -20,7 +20,7 @@ namespace CM.Data.ViewModels.Appointment
         public Gender Gender { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "This Field is requiered")]
-        [RegularExpression(@"^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$", ErrorMessage = "Please Provide Valid Phone Numbers")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Please Provide Valid Phone Numbers")]
         public string Phone { get; set; }
 
         [Display(Name ="Email-ID")]
---
+++ ClinicManagement/CM.Web/Areas/Admin/Controllers/MedicineController.cs
@@ -0,0 +1,37 @@
+﻿using CM.Web.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CM.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class MedicineController : BaseController
+    {
+        // GET: Admin/Medicine
+        public ActionResult Index()
+        {
+            return RedirectToAction("ViewMedicines");
+        }
+
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(string a)
+        {
+            return View();
+        }
+
+        [ActionName("View")]
+        public ActionResult ViewMedicines()
+        {
+            return View();
+        }
+
+    }
+}
