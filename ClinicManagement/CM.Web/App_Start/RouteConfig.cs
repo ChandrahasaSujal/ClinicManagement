7aaac4bd5aa3ed10f055ca27938bfaf54040a0f0
Chandrahasa Sujal <chanducodemonk@outlook.com>
2020-01-14 23:41:15 +0530

    Login and redirecting to home page on remember me has implemented.

--- ClinicManagement/CM.Web/App_Start/RouteConfig.cs
+++ ClinicManagement/CM.Web/App_Start/RouteConfig.cs
@@ -17,8 +17,8 @@ namespace CM.Web
             routes.MapRoute(
                  name: "Default",
                  url: "{controller}/{action}/{id}",
-                 defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
-                 namespaces: new string[] { "CM.Web.Controllers" }
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                 namespaces: new string[] { "CM.Web.Areas.Admin.Controllers" }
              ).DataTokens["Area"] = "Admin";
         }
     }
